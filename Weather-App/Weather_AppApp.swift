bb6983caaae2a8965508101d5558d9e2d91addbe
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-10-21 11:20:16 +0200

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Bilal Dallali on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
