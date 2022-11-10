f4090f70f1d9026a44ff4f74a29aaf9c5cac816e
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-11-10 19:15:13 +0100

    adding weatherbutton in its own file

---
+++ Weather-App/WeatherButton.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherButton.swift
+//  Weather-App
+//
+//  Created by Bilal Dallali on 10/11/2022.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(5.0)
+    }
+}
